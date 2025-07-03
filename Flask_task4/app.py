2b6c6347e6e95e105909e2dfb21a6502d91b3b04
Ahmed Hossam <107175822+AhmedHossamAshour@users.noreply.github.com>
2025-07-03 20:33:05 +0300

    tsak4

---
+++ Flask_task4/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Book
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'crud_secret'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    books = Book.query.all()
+    return render_template('index.html', books=books)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        new_book = Book(title=title, author=author)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add_book.html')
+
+@app.route('/update/<int:book_id>', methods=['GET', 'POST'])
+def update_book(book_id):
+    book = Book.query.get_or_404(book_id)
+    if request.method == 'POST':
+        book.title = request.form['title']
+        book.author = request.form['author']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update_book.html', book=book)
+
+@app.route('/delete/<int:book_id>')
+def delete_book(book_id):
+    book = Book.query.get_or_404(book_id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
